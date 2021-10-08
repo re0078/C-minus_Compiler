b3be832f988b4629775f10303a1fbda39d3e7de4
reza <reza.abdollahzadeh12@gmail.com>
2021-10-08 22:38:54 +0330

    file interactions added\ general structure improved

--- scanner.py
+++ scanner.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from sys import argv
 
 num_set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 alphanum_set = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K',
@@ -17,31 +18,69 @@ class TokenType(Enum):
     SYMBOL = (4, symbols_set),
     COMMENT = (5, comment_set),
     WHITESPACE = (6, whitespaces_set)
+    EOF = (7, [])
 
 
-def get_next_token(seq: str) -> (Enum, str):
-    return
+cur_seq = ""
+remained_char = None
 
 
-def parse_num(seq: str):
+# TODO decide for handling errors (can it be done in get_next_token or it has to be done in scan methods?)
+def get_next_token(file) -> (bool, Enum, str):
+    global remained_char
+    c = file.read(1)  # initial character
+    if not c:
+        return True, TokenType.EOF, ""
+
+    #  TODO switch-case like logic
+    # seq, accepted, remained_char = scan_comment(c, f)
+
+
+# general pattern
+def scan_for_num(initial_char: str, f) -> (str, bool, str):
+    while True:
+        c = f.read(1)
+    pass
+
+
+def scan_for_id_keyword(seq: str) -> (str, bool, str):
     return seq
 
 
-def parse_id(seq: str):
+def scan_for_symbol(seq: str) -> (str, bool, str):
     return seq
 
 
-def parse_keyword(seq: str):
+def scan_comment(seq: str) -> (str, bool, str):
     return seq
 
 
-def parse_symbol(seq: str):
+def scan_for_whitespace(seq: str) -> (str, bool, str):
     return seq
 
 
-def parse_comment(seq: str):
-    return seq
+def dprint(*string):  # debug print
+    global DEBUG
+    if DEBUG:
+        print(*string)
 
 
-def parse_whitespace(seq: str):
-    return seq
+file_name = argv[1]
+line_idx = 1
+DEBUG = False
+
+
+def __main__():
+    global DEBUG
+    if len(argv) >= 3:
+        DEBUG = bool(argv[2])
+    with open(file_name, 'r') as f, open('tokens.txt', 'w') as tokens_file, \
+            open('lexical_errors.txt', 'w') as errors_file, open('symbol_table.txt', 'w') as symbol_table_file:
+        while True:
+            eof, token, seq = get_next_token(f)
+            if not eof:
+                tokens_file.write(str((token, seq)) + ',')
+                if token is TokenType.KEYWORD or token is TokenType.ID:
+                    symbol_table_file.write(seq + ',\n')
+            else:
+                dprint("End of compilation.")
