0ef84326a48dca0c59ee364b1227ed2d964d4738
reza <reza.abdollahzadeh12@gmail.com>
2021-10-10 10:13:36 +0330

    get_next_token loop added \ general refactor

--- scanner.py
+++ scanner.py
@@ -1,39 +1,14 @@
 from enum import Enum
 from sys import argv
 
-num_set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-alphanum_set = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K',
+num_set = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+alphanum_set = {'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K',
                 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u',
-                'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z'] + num_set
-keywords_set = ['if', 'else', 'return', 'break', 'until', 'repeat', 'int', 'void']
-symbols_set = [';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '*', '=', '<']
-comment_set = ['/*', '//']
-whitespaces_set = [' ', '\n', '\r', '\t', '\v', '\f']
-
-
-class TokenType(Enum):
-    NUM = (1, num_set),
-    ID = (2, alphanum_set),
-    KEYWORD = (3, keywords_set),
-    SYMBOL = (4, symbols_set),
-    COMMENT = (5, comment_set),
-    WHITESPACE = (6, whitespaces_set)
-    EOF = (7, [])
-
-
-cur_seq = ""
-remained_char = None
-
-
-# TODO decide for handling errors (can it be done in get_next_token or it has to be done in scan methods?)
-def get_next_token(file) -> (bool, Enum, str):
-    global remained_char
-    c = file.read(1)  # initial character
-    if not c:
-        return True, TokenType.EOF, ""
-
-    #  TODO switch-case like logic
-    # seq, accepted, remained_char = scan_comment(c, f)
+                'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z'}.union(num_set)
+keywords_set = {'if', 'else', 'return', 'break', 'until', 'repeat', 'int', 'void'}
+symbols_set = {';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '*', '=', '<'}
+comment_set = {'/*', '//'}
+whitespaces_set = {' ', '\n', '\r', '\t', '\v', '\f'}
 
 
 # general pattern
@@ -43,7 +18,7 @@ def scan_for_num(initial_char: str, f) -> (str, bool, str):
     pass
 
 
-def scan_for_id_keyword(seq: str) -> (str, bool, str):
+def scan_for_id_keyword(seq: str, f) -> (str, bool, str):
     return seq
 
 
@@ -57,6 +32,44 @@ def scan_comment(seq: str) -> (str, bool, str):
 
 def scan_for_whitespace(seq: str) -> (str, bool, str):
     return seq
+
+
+class TokenType(Enum):
+    # id, initial_set, vocabulary, scan_func
+    NUM = (1, num_set, num_set, scan_for_num),
+    ID = (2, alphanum_set, alphanum_set + num_set, scan_for_id_keyword),
+    KEYWORD = (3, alphanum_set, keywords_set, scan_for_id_keyword),
+    SYMBOL = (4, symbols_set, symbols_set, scan_for_symbol),
+    COMMENT = (5, {'/'}, comment_set, scan_comment),
+    WHITESPACE = (6, whitespaces_set, whitespaces_set, scan_for_whitespace)
+    EOF = (7, {}, {})
+
+
+_cur_seq = ""
+_remained_char = None
+
+
+# TODO decide for handling errors (can it be done in get_next_token or it has to be done in scan methods?)
+def get_next_token(file) -> (bool, Enum, str):
+    global _cur_seq, _remained_char
+    c = file.read(1)  # initial character
+    if not c:
+        return True, TokenType.EOF, ""
+
+    for token_type in TokenType:
+        initial_set = token_type.value[1]
+        if c in initial_set:
+            scan_func = token_type.value[3]
+            # scan function is reading, token accepted, result token(if accepted), extra char
+            read, accepted, seq, _remained_char = scan_func(c, file)
+            while read:
+                _cur_seq += c
+                if accepted:
+                    return False, token_type, seq
+                c = file.read(1)
+                if not c:
+                    return True, TokenType.EOF, ""
+                read, accepted, seq, _remained_char = scan_func(c, file)
 
 
 def dprint(*string):  # debug print
@@ -79,6 +92,8 @@ def __main__():
         while True:
             eof, token, seq = get_next_token(f)
             if not eof:
+                # todo handle remained char
+                #
                 tokens_file.write(str((token, seq)) + ',')
                 if token is TokenType.KEYWORD or token is TokenType.ID:
                     symbol_table_file.write(seq + ',\n')
