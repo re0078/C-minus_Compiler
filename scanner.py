3cc69cf16389109829e566e8d203fec2a5c9fa4f
reza <reza.abdollahzadeh12@gmail.com>
2021-10-11 19:48:15 +0330

    minor refactors

--- scanner.py
+++ scanner.py
@@ -2,9 +2,9 @@ from enum import Enum
 from sys import argv
 
 num_set = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
-alphanum_set = {'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K',
+alphabet_set = {'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K',
                 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u',
-                'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z'}.union(num_set)
+                'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z'}
 keywords_set = {'if', 'else', 'return', 'break', 'until', 'repeat', 'int', 'void'}
 symbols_set = {';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '*', '=', '<'}
 comment_set = {'/*', '//'}
@@ -12,33 +12,31 @@ whitespaces_set = {' ', '\n', '\r', '\t', '\v', '\f'}
 
 
 # general pattern
-def scan_for_num(initial_char: str, f) -> (str, bool, str):
-    while True:
-        c = f.read(1)
+def scan_for_num(c: str) -> (str, bool, str):
     pass
 
 
-def scan_for_id_keyword(seq: str, f) -> (str, bool, str):
-    return seq
+def scan_for_id_keyword(c: str, f) -> (str, bool, str):
+    return c
 
 
-def scan_for_symbol(seq: str) -> (str, bool, str):
-    return seq
+def scan_for_symbol(c: str) -> (str, bool, str):
+    return c
 
 
-def scan_comment(seq: str) -> (str, bool, str):
-    return seq
+def scan_comment(c: str) -> (str, bool, str):
+    return c
 
 
-def scan_for_whitespace(seq: str) -> (str, bool, str):
-    return seq
+def scan_for_whitespace(c: str) -> (str, bool, str):
+    return c
 
 
 class TokenType(Enum):
     # id, initial_set, vocabulary, scan_func
     NUM = (1, num_set, num_set, scan_for_num),
-    ID = (2, alphanum_set, alphanum_set + num_set, scan_for_id_keyword),
-    KEYWORD = (3, alphanum_set, keywords_set, scan_for_id_keyword),
+    ID = (2, alphabet_set, alphabet_set.union(num_set), scan_for_id_keyword),
+    KEYWORD = (3, alphabet_set, keywords_set, scan_for_id_keyword),
     SYMBOL = (4, symbols_set, symbols_set, scan_for_symbol),
     COMMENT = (5, {'/'}, comment_set, scan_comment),
     WHITESPACE = (6, whitespaces_set, whitespaces_set, scan_for_whitespace)
@@ -52,6 +50,7 @@ _remained_char = None
 # TODO decide for handling errors (can it be done in get_next_token or it has to be done in scan methods?)
 def get_next_token(file) -> (bool, Enum, str):
     global _cur_seq, _remained_char
+    # todo handle remained char here
     c = file.read(1)  # initial character
     if not c:
         return True, TokenType.EOF, ""
@@ -61,7 +60,7 @@ def get_next_token(file) -> (bool, Enum, str):
         if c in initial_set:
             scan_func = token_type.value[3]
             # scan function is reading, token accepted, result token(if accepted), extra char
-            read, accepted, seq, _remained_char = scan_func(c, file)
+            read, accepted, seq, _remained_char = scan_func(c)
             while read:
                 _cur_seq += c
                 if accepted:
@@ -69,7 +68,7 @@ def get_next_token(file) -> (bool, Enum, str):
                 c = file.read(1)
                 if not c:
                     return True, TokenType.EOF, ""
-                read, accepted, seq, _remained_char = scan_func(c, file)
+                read, accepted, seq, _remained_char = scan_func(c)
 
 
 def dprint(*string):  # debug print
@@ -99,3 +98,4 @@ def __main__():
                     symbol_table_file.write(seq + ',\n')
             else:
                 dprint("End of compilation.")
+                break
